7a169ba9e3632ce7bbb7df76af36f14bf06028a4
Rohan Sadhwani <rohansadhwani5@gmail.com>
2026-01-29 20:54:57 +0530

    Minor Bug Fixes

--- Dockerfile
+++ Dockerfile
@@ -11,7 +11,7 @@ RUN apk add --no-cache openssl
 COPY package*.json ./
 
 # Install dependencies
-RUN npm install
+RUN npm ci
 
 # Copy rest of the project
 COPY . .
--- server.js
+++ server.js
@@ -48,8 +48,8 @@ const startServer = async () => {
     // Connect to PostgreSQL
     await connectDB();
 
-    app.listen(PORT, () => {
-      console.log(`🚀 Server running on http://localhost:${PORT}`);
+    app.listen(PORT, "0.0.0.0", () => {
+      console.log(`🚀 Server running on port ${PORT}`);
     });
   } catch (err) {
     console.error("❌ Failed to start server:", err.message);
